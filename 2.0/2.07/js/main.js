2945de44bea220894e29db62e9667fb74ef47eb9
Dave <dave.gagnat@gmail.com>
2020-03-21 23:16:27 -0500

    Converted age data from string to integers

--- 2.0/2.07/js/main.js
+++ 2.0/2.07/js/main.js
@@ -4,6 +4,15 @@
 *    2.7 - Loading external data
 */
 
-d3.csv('data/ages.csv').then((data) => {
+d3.tsv('data/ages.tsv').then((data) => {
+    data.forEach((d) => {
+        d.age = +d.age; // putting + in front, converts to integer
+    })
+
     console.log(data);
 });
+
+
+
+
+
