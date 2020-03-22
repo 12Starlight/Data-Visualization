e590cbbd44d0cb12c963fb01d053205c3327dfd1
Dave <dave.gagnat@gmail.com>
2020-03-21 23:27:53 -0500

    Got x circles representing the length of the array, then made the radius the size of the age * 2 and changed Tony

--- 2.0/2.07/js/main.js
+++ 2.0/2.07/js/main.js
@@ -9,7 +9,30 @@ d3.tsv('data/ages.tsv').then((data) => {
         d.age = +d.age; // putting + in front, converts to integer
     })
 
-    console.log(data);
+    const svg = d3.select('#chart-area').append('svg')
+        .attr('width', 400)
+        .attr('height', 400);
+
+    const circles = svg.selectAll('circle')
+        .data(data)
+
+    circles.enter()
+        .append('circle')
+            .attr('cx', (d, i) => {
+                return (i * 50) + 25;
+            })
+            .attr('cy', 25)
+            .attr('r', (d) => {
+                console.log(d); 
+                return d.age * 2;
+            })
+            .attr('fill', (d) => {
+                if (d.name == 'Tony') {
+                    return 'blue'
+                } else {
+                    return 'red'
+                }
+            })
 });
 
 
