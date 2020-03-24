92fa051c77b7f563705469dd8e808f0b9f862af3
Dave <dave.gagnat@gmail.com>
2020-03-23 22:34:59 -0500

    Tested a rectangle position and got back the pixal cordinate for where the x cordinate for the element is positionined

--- 3.0/3.06/js/main.js
+++ 3.0/3.06/js/main.js
@@ -4,48 +4,47 @@
 *    3.6 - Band scales
 */
 
-const svg = d3.select("#chart-area")
+var svg = d3.select("#chart-area")
     .append("svg")
     .attr("width", "400")
     .attr("height", "400");
 
-d3.json('data/buildings.json').then((data) => {
+d3.json("data/buildings.json").then((data) => {
     console.log(data);
 
-    data.forEach(d => {
-        d.height = +d.height 
+    data.forEach((d) => {
+        d.height = +d.height;
     });
 
-    const x = d3.scaleBand() 
-        .domain(
-            ['Burj Khalifa', 
-            'Shanghai Tower', 
-            'Abraj Al-Bait Clock Tower', 
-            'Ping An Finance Center',
-            'Lotte World Tower'])
+    const x = d3.scaleBand()
+        .domain(["Burj Khalifa", "Shanghai Tower", 
+            "Abraj Al-Bait Clock Tower", "Ping An Finance Centre", 
+            "Lotte World Tower", "One World Trade Center",
+            "Guangzhou CTF Finance Center"])
         .range([0, 400])
-        .paddingInner(0.2)
-        .paddingOuter(0.2); 
+        .paddingInner(0.3)
+        .paddingOuter(0.3);
 
-    console.log(x('Burj Khalifa'))
+    console.log(x('Burj Khalifa'));
 
     const y = d3.scaleLinear()
         .domain([0, 828])
         .range([0, 400]);
 
-    const rects = svg.selectAll('rect')
-        .data(data)
+    const rects = svg.selectAll("rect")
+            .data(data)
         .enter()
-            .append('rect')
-            .attr('y', 20)
-            .attr('x', (d, i) => {
-                return i * 60;
+            .append("rect")
+            .attr("y", 0)
+            .attr("x", (d) => {
+                return x(d.name);
             })
-            .attr('width', 40)
-            .attr('height', (d) => {
+            .attr("width", x.bandwidth)
+            .attr("height", (d) => {
                 return y(d.height);
             })
-            .attr('fill', (d) => {
-                return 'grey';
+            .attr("fill", (d) => {
+                return "grey";
             });
+
 })
