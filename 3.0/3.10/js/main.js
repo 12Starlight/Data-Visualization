dd2355341e63aa871e99f2b51cd48bd755e3c217
Dave <dave.gagnat@gmail.com>
2020-03-25 01:06:44 -0500

    Made chart to include margins and used to create axis for x and y

---
+++ 3.0/3.10/js/main.js
@@ -0,0 +1,60 @@
+/*
+*    main.js
+*    Mastering Data Visualization with D3.js
+*    3.10 - Axes and labels
+*/
+
+const margin = { left: 100, right: 10, top: 10, bottom: 150 }
+
+const width = 600 - margin.left - margin.right; 
+const height = 400 - margin.top - margin.bottom; 
+
+
+const g = d3.select('#chart-area')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+
+d3.json('data/buildings.json').then((data) => {
+    console.log(data);
+
+    data.forEach((d) => {
+        d.height = +d.height;
+    });
+
+    const x = d3.scaleBand()
+        .domain(data.map((d) => {
+            return d.name;
+        }))
+        .range([0, width])
+        .paddingInner(0.3)
+        .paddingOuter(0.3);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, (d) => {
+            return d.height; 
+        })])
+        .range([0, height]);
+
+    const xAxisCall = d3.axisBottom(x);
+        g.append('g')
+            .attr('class', 'x-axis')
+            .attr('transform', 'translate(0, ' + height + ')')
+            .call(xAxisCall)
+            .selectAll('text')
+                .attr('y', '10')
+                .attr('x', '-5')
+                .attr('text-anchor', 'end')
+                .attr('transform', 'rotate(-40)')
+
+    const yAxisCall = d3.axisLeft(y);
+        g.append('g')
+            .attr('class', 'y-axis')
+            .call(yAxisCall);
+
+    
+    const rects = g.selectAll('rect')
+        .data(data)
+})
