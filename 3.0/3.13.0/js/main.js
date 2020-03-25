605342edb66dbb4b5c2c90570c46aa9a049d4d3b
Dave <dave.gagnat@gmail.com>
2020-03-25 03:45:23 -0500

    Successfully pulled external data and changed numerical string types to integer types

--- 3.0/3.13.0/js/main.js
+++ 3.0/3.13.0/js/main.js
@@ -3,3 +3,36 @@
 *    Mastering Data Visualization with D3.js
 *    Project 1 - Star Break Coffee
 */
+
+// Build Margin
+const margin = { left: 50, right: 20, top: 10, bottom: 30 };
+
+// Incorporate Margin
+const width = 600 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
+
+// Create SVG, Group SVG
+const g = d3.select('#chart-area').append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  // group to translate or move svg according to margins
+  .append('g')
+  .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')  
+
+// X Label
+g.append('text')
+
+// Y Label
+g.append('text')
+
+// Get External Data
+d3.json('data/revenues.json').then((data) => {
+  console.log(data)
+
+  data.forEach((d) => {
+    d.revenue = +d.revenue;
+    d.profit = +d.profit; 
+  })
+
+
+})
