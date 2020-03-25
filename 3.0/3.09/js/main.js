c398310877a5823fae75f9542154d2dc44903db5
Dave <dave.gagnat@gmail.com>
2020-03-24 19:44:28 -0500

    Used d3 to create a margin using an object as the margin

---
+++ 3.0/3.09/js/main.js
@@ -0,0 +1,53 @@
+/*
+*    main.js
+*    Mastering Data Visualization with D3.js
+*    3.9 - Margins and groups
+*/
+
+const margin = { left: 100, right: 10, top: 10, bottom: 100 };
+
+const width = 600 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
+
+
+const svg = d3.select('#chart-area')
+    .append('svg')
+    .attr('width', '400')
+    .attr('height', '400')
+
+d3.json('data/buildings.json').then((data) => {
+    console.log(data);
+
+    data.forEach((d) => {
+        return d.height = +d.height; 
+    })
+
+    const x = d3.scaleBand()
+        .domain(data.map((d) => {
+            return d.name;
+        }))
+        .range([0, 400])
+        .paddingInner(0.3)
+        .paddingOuter(0.3);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, (d) => {
+            return d.height;
+        })])
+        .range([0, 400]);
+
+    var rects = svg.selectAll('rect')
+        .data(data)
+    
+    rects.enter()
+        .append('rect')
+        .attr('y', 0)
+        .attr('x', (d) => {
+            return x(d.name);
+        })
+        .attr('width', x.bandwidth)
+        .attr('height', (d) => {
+            return y(d.height);
+        })
+        .attr('fill', 'grey')
+})
