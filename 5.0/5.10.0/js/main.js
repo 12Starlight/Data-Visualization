fee8d85d9b1313b92f3b8e8e59aebc288f88bd56
Dave <dave.gagnat@gmail.com>
2020-03-30 05:16:27 -0500

    Built scales and lables for scatter plot project

--- 5.0/5.10.0/js/main.js
+++ 5.0/5.10.0/js/main.js
@@ -8,8 +8,11 @@
 const margin = { left: 80, right: 20, top: 50, bottom: 100 };
 
 // Incorporate Margin
-const width = 600 - margin.left - margin.right;
-const height = 400 - margin.top - margin.bottom;
+const width = 800 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+
+// Create Time Variable
+const time = 0; 
 
 // Create Boolean Flag
 let flag = true;
@@ -25,48 +28,55 @@ const g = d3.select('#chart-area')
 		.append('g')
 		.attr('transform', 'translate(' +  margin.left + ', ' + margin.top + ')');
 
-// X Group
-const xAxisGroup = g.append('g')
-	.attr('class', 'x axis')
-	.attr('transform', 'translate(0, ' + height + ')');
-
-// Y Group
-const yAxisGroup = g.append('g')
-	.attr('class', 'y axis');
-
 // X Scale 
-const x = d3.scaleBand()
+const x = d3.scaleLog()
+	.base(10)
 	.range([0, width])
-	.padding(0.2);
+	.domain([142, 150000])
 
 // Y Scale
 const y = d3.scaleLinear()
 	.range([height, 0])
+	.domain([0, 90]);
+
+// Area Scale 
+const area = d3.scaleLinear()
+	.range([25 * Math.PI, 1500 * Math.PI])
+	.domain([2000, 1400000000]);
+
+const continentColor = d3.scaleOrdinal(d3.schemePaste1); 
 
 // X Label
-g.append('text')
+let xLabel = g.append('text')
 	.attr('y', height + 50)
 	.attr('x', width / 2)
 	.attr('font-size', '20px')
 	.attr('text-anchor', 'middle')
-	.text('GDP Per Capita');
+	.text('GDP Per Capita ($)');
 	
 // Y Label
 let yLabel = g.append('text')
-	.attr('y', -60)
-	.attr('x', -(height / 2))
+	.attr('y', -40)
+	.attr('x', -170)
 	.attr('font-size', '20px')
 	.attr('text-anchor', 'middle')
 	.attr('transform', 'rotate(-90)')
-	.text('Life Expectancy')
+	.text('Life Expectancy (Years)')
+
+// Time Label
+let timeLabel = g.append('text')
+	.attr('y', height - 10)
+	.attr('x', width - 40)
+	.attr('font-size', '40px')
+	.attr('opacity', '0.4')
+	.attr('text-anchor', 'middle')
+	.text('1800')
 
 // Retrieve Data
 d3.json("data/data.json").then((data) => {
 	console.log(data);
 
 	data.forEach((year) => {
-		let newData = year.countries.filter((value) => value === null)
-		console.log(yea);
-		 
+
 	})
 })
