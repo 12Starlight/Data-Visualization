18577e7a6bbbf1bf2acc186c71df7135e1467a9b
Dave <dave.gagnat@gmail.com>
2020-03-31 17:16:56 -0500

    Started to parse through data and format data. Then converted file to ES6 where applicable

--- 6.0/6.10.0/js/main.js
+++ 6.0/6.10.0/js/main.js
@@ -4,110 +4,151 @@
 *    CoinStats
 */
 
-var margin = { left:80, right:100, top:50, bottom:100 },
-    height = 500 - margin.top - margin.bottom, 
-    width = 800 - margin.left - margin.right;
+// Build Margin
+const margin = { left:80, right:100, top:50, bottom:100 }
 
-var svg = d3.select("#chart-area").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
+// Incorporate Margin
+const height = 500 - margin.top - margin.bottom;
+const width = 800 - margin.left - margin.right;
 
-var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + 
-        ", " + margin.top + ")");
+// Create Time Variable
+let time = 0; 
+
+// Create Interval Variable
+let formattedData;
+
+// Create Boolean Flag
+let flat = true;
+
+// Build SVG, Group SVG
+const svg = d3.select('#chart-area').append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom);
+
+const g = svg.append('g')
+    .attr('transform', 'translate(' + margin.left + 
+        ', ' + margin.top + ')');
 
 // Time parser for x-scale
-var parseTime = d3.timeParse("%Y");
+const parseTime = d3.timeParse('%Y');
+
 // For tooltip
-var bisectDate = d3.bisector(function(d) { return d.year; }).left;
+const bisectDate = d3.bisector((d) => { 
+        return d.year; 
+    }).left;
 
 // Scales
-var x = d3.scaleTime().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
+const x = d3.scaleTime().range([0, width]);
+const y = d3.scaleLinear().range([height, 0]);
 
 // Axis generators
-var xAxisCall = d3.axisBottom()
-var yAxisCall = d3.axisLeft()
+const xAxisCall = d3.axisBottom()
+const yAxisCall = d3.axisLeft()
     .ticks(6)
-    .tickFormat(function(d) { return parseInt(d / 1000) + "k"; });
+    .tickFormat((d) => { 
+        return parseInt(d / 1000) + 'k'; 
+    });
 
 // Axis groups
-var xAxis = g.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")");
-var yAxis = g.append("g")
-    .attr("class", "y axis")
+const xAxis = g.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + height + ')');
+const yAxis = g.append('g')
+    .attr('class', 'y axis')
     
 // Y-Axis label
-yAxis.append("text")
-    .attr("class", "axis-title")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .attr("fill", "#5D6971")
-    .text("Population)");
+yAxis.append('text')
+    .attr('class', 'axis-title')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', 6)
+    .attr('dy', '.71em')
+    .style('text-anchor', 'end')
+    .attr('fill', '#5D6971')
+    .text('Population)');
 
 // Line path generator
-var line = d3.line()
-    .x(function(d) { return x(d.year); })
-    .y(function(d) { return y(d.value); });
+let line = d3.line()
+    .x((d) => { 
+        return x(d.year); 
+    })
+    .y((d) => { 
+        return y(d.value); 
+    });
 
-d3.json("data/coins.json").then(function(data) {
+d3.json('data/coins.json').then((data) => {
     console.log(data); 
 
     // Data cleaning
     const keys = Object.keys(data);
     console.log(keys);
 
+    let formattedData = keys.map((key) => {
+
+        return data[key].filter((crypto) => {
+            let dataExists = (crypto['24h_vol'] && crypto.date && crypto.market_cap && crypto.price_usd)
+            return dataExists;
+        }).map((crypto) => {
+            crypto['24h_vol'] = +crypto['24h_vol'];
+
+            return crypto; 
+        })
+    })
+
+    console.log(formattedData); 
 
     // Set scale domains
-    x.domain(d3.extent(data, function(d) { return d.year; }));
-    y.domain([d3.min(data, function(d) { return d.value; }) / 1.005, 
-        d3.max(data, function(d) { return d.value; }) * 1.005]);
+    x.domain(d3.extent(data, (d) => { 
+        return d.year; 
+    }));
+    y.domain([d3.min(data, (d) => { 
+        return d.value; 
+    }) / 1.005, 
+        d3.max(data, (d) => { 
+            return d.value; 
+        }) * 1.005]);
 
     // Generate axes once scales have been set
     xAxis.call(xAxisCall.scale(x))
     yAxis.call(yAxisCall.scale(y))
 
     // Add line to chart
-    g.append("path")
-        .attr("class", "line")
-        .attr("fill", "none")
-        .attr("stroke", "grey")
-        .attr("stroke-with", "3px")
-        .attr("d", line(data));
+    g.append('path')
+        .attr('class', 'line')
+        .attr('fill', 'none')
+        .attr('stroke', 'grey')
+        .attr('stroke-with', '3px')
+        .attr('d', line(data));
 
     /******************************** Tooltip Code ********************************/
 
-    var focus = g.append("g")
-        .attr("class", "focus")
-        .style("display", "none");
+    const focus = g.append('g')
+        .attr('class', 'focus')
+        .style('display', 'none');
 
-    focus.append("line")
-        .attr("class", "x-hover-line hover-line")
-        .attr("y1", 0)
-        .attr("y2", height);
+    focus.append('line')
+        .attr('class', 'x-hover-line hover-line')
+        .attr('y1', 0)
+        .attr('y2', height);
 
-    focus.append("line")
-        .attr("class", "y-hover-line hover-line")
-        .attr("x1", 0)
-        .attr("x2", width);
+    focus.append('line')
+        .attr('class', 'y-hover-line hover-line')
+        .attr('x1', 0)
+        .attr('x2', width);
 
-    focus.append("circle")
-        .attr("r", 7.5);
+    focus.append('circle')
+        .attr('r', 7.5);
 
-    focus.append("text")
-        .attr("x", 15)
-        .attr("dy", ".31em");
+    focus.append('text')
+        .attr('x', 15)
+        .attr('dy', '.31em');
 
-    g.append("rect")
-        .attr("class", "overlay")
-        .attr("width", width)
-        .attr("height", height)
-        .on("mouseover", function() { focus.style("display", null); })
-        .on("mouseout", function() { focus.style("display", "none"); })
-        .on("mousemove", mousemove);
+    g.append('rect')
+        .attr('class', 'overlay')
+        .attr('width', width)
+        .attr('height', height)
+        .on('mouseover', () => focus.style('display', null))
+        .on('mouseout', () => focus.style('display', 'none'))
+        .on('mousemove', mousemove);
 
     function mousemove() {
         var x0 = x.invert(d3.mouse(this)[0]),
@@ -115,10 +156,10 @@ d3.json("data/coins.json").then(function(data) {
             d0 = data[i - 1],
             d1 = data[i],
             d = x0 - d0.year > d1.year - x0 ? d1 : d0;
-        focus.attr("transform", "translate(" + x(d.year) + "," + y(d.value) + ")");
-        focus.select("text").text(d.value);
-        focus.select(".x-hover-line").attr("y2", height - y(d.value));
-        focus.select(".y-hover-line").attr("x2", -x(d.year));
+        focus.attr('transform', 'translate(' + x(d.year) + ',' + y(d.value) + ')');
+        focus.select('text').text(d.value);
+        focus.select('.x-hover-line').attr('y2', height - y(d.value));
+        focus.select('.y-hover-line').attr('x2', -x(d.year));
     }
 
 
