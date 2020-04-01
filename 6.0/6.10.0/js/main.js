594b109a405a57ed46369fb53f2d2b5e1f4764e8
Dave <dave.gagnat@gmail.com>
2020-04-01 01:47:02 -0500

    Built crypto chart

--- 6.0/6.10.0/js/main.js
+++ 6.0/6.10.0/js/main.js
@@ -49,9 +49,8 @@ let bisectDate = d3.bisector((d) => {
 g.append('path')
     .attr('class', 'line')
     .attr('fill', 'none')
-    .attr('stroke', 'grey')
-    .attr('stroke-with', '3px')
-
+    .attr('stroke', 'blue')
+    .attr('stroke-width', '3px')
 
 // X-Axis Label
 let xLabel = g.append('text')
@@ -60,6 +59,8 @@ let xLabel = g.append('text')
     .attr('x', width / 2)
     .attr('font-size', '20px')
     .attr('text-anchor', 'middle')
+    .attr('font-weight', '500')
+    .attr('fill', '#5D6971')
     .text('Time');
 
 // Y-Axis Label
@@ -93,107 +94,142 @@ const xAxis = g.append('g')
 // Axis groups y
 const yAxis = g.append('g')
     .attr('class', 'y axis')
-    
 
-
-// Line path generator
-let line = d3.line()
-    .x((d) => { 
-        return x(parseTime(d.date)); 
-    })
-    .y((d) => { 
-        return y(d.price_usd); 
-    });
+// Add jQuery UI Slider
+$('#date-slider').slider({
+    range: true,
+    max: parseTime('31/10/2017').getTime(),
+    min: parseTime('12/5/2013').getTime(),
+    step: 86400000, // One Day
+    values: [parseTime('12/5/2013').getTime(), parseTime('31/10/2017').getTime()],
+    slide: (event, ui) => {
+        $('#dateLabel1').text(formatTime(new Date(ui.values[0])));
+        $('#dateLabel2').text(formatTime(new Date(ui.values[1])));
+        update();
+    }
+});
 
 d3.json('data/coins.json').then((data) => {
     console.log(data); 
 
     // Data cleaning
-    const keys = Object.keys(data);
-    console.log(keys);
-
-    formattedData = keys.map((key) => {
-
-        return data[key].filter((crypto) => {
-            let dataExists = (crypto['24h_vol'] && crypto.date && crypto.market_cap && crypto.price_usd)
-            return dataExists;
-        }).map((crypto) => {
-            crypto['24h_vol'] = +crypto['24h_vol'];
-            crypto.market_cap = +crypto.market_cap;
-            crypto.price_usd = +crypto.price_usd; 
-            
-            return crypto; 
+    filteredData = {};
+    for (let coin in data) {
+        if (!data.hasOwnProperty(coin)) {
+            continue;
+        }
+        filteredData[coin] = data[coin].filter((d) => {
+            return !(d.price_usd === null)
         })
-    })
+        filteredData[coin].forEach((d) => {
+            d.price_usd = +d.price_usd;
+            d['24h_vol'] = +d['24h_vol'];
+            d.market_cap = +d.market_cap;
+            d.date = parseTime(d.date)
+        });
+    }
 
-    console.log(formattedData); 
-    console.log(formattedData[0].map((d) => {
-        return d.price_usd; 
-    }));
+    // Run the visualization for the first time
+    update();
+});
 
-    // Set scale domains
-    x.domain(d3.extent(formattedData[0], (d) => { 
-        return parseTime(d.date); 
-    }));
-    y.domain([d3.min(formattedData[0], (d) => { 
-        return d.price_usd; 
-    }), 
-        d3.max(formattedData[0], (d) => { 
-            return d.price_usd; 
-        })]);
+const update = () => {
+    // Filter data based on selections
+    let coin = $('#coin-select').val(),
+        yValue = $('#var-select').val(),
+        sliderValues = $('#date-slider').slider('values'); 
+    let dataTimeFiltered = filteredData[coin].filter((d) => {
+        return ((d.date >= sliderValues[0]) && (d.date <= sliderValues[1]))
+    });
 
-    // Generate axes once scales have been set
-    xAxis.call(xAxisCall.scale(x))
-    yAxis.call(yAxisCall.scale(y))
+    // Update Scales
+    x.domain(d3.extent(dataTimeFiltered, (d) => d.date));
+    y.domain([d3.min(dataTimeFiltered, (d) => d[yValue]), 
+        d3.max(dataTimeFiltered, (d) => d[yValue])]);
 
+    // Fix for format values
+    let formatSi = d3.format('.2s');
 
-        .attr('d', line(formattedData[0]));
+    let formatAbbreviation = (x) => {
+        let s = formatSi(x);
+        switch (s[s.length - 1]) { 
+            case 'G': return s.slice(0, -1) + 'B';
+            case 'k': return s.slice(0, -1) + 'K';
+        }
 
-    /******************************** Tooltip Code ********************************/
+        return s; 
+    }
 
-    const focus = g.append('g')
+    // Update Axes
+    xAxisCall.scale(x);
+    xAxis.transition(t()).call(xAxisCall);
+    yAxisCall.scale(y);
+    yAxis.transition(t()).call(yAxisCall.tickFormat(formatAbbreviation));
+
+    // Clear Old ToolTips
+    d3.select('.focus').remove();
+    d3.select('.overlay').remove();
+
+    // Create Mouse Move
+    function mousemove() {
+        let x0 = x.invert(d3.mouse(this)[0]),
+            i = bisectDate(dataTimeFiltered, x0, 1),
+            d0 = dataTimeFiltered[i - 1],
+            d1 = dataTimeFiltered[i],
+            d = (d1 && d0) ? (x0 - d0.date > d1.date - x0 ? d1 : d0) : 0;
+        focus.attr('transform', 'translate(' + x(d.date) + ', ' + y(d[yValue]) + ')');
+        focus.select('text').text(() => d3.format('$,')(d[yValue].toFixed(2)));
+        focus.select('.x-hover-line').attr('y2', height - y(d[yValue]));
+        focus.select('.y-hover-line').attr('x2', -x(d.date));
+
+    // ToolTip Code
+    var focus = g.append('g')
         .attr('class', 'focus')
         .style('display', 'none');
-
     focus.append('line')
         .attr('class', 'x-hover-line hover-line')
         .attr('y1', 0)
         .attr('y2', height);
-
     focus.append('line')
         .attr('class', 'y-hover-line hover-line')
         .attr('x1', 0)
         .attr('x2', width);
-
     focus.append('circle')
-        .attr('r', 7.5);
-
+        .attr('r', 5);
     focus.append('text')
         .attr('x', 15)
         .attr('dy', '.31em');
-
-    g.append('rect')
+    svg.append('rect')
+        .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
         .attr('class', 'overlay')
         .attr('width', width)
         .attr('height', height)
-        .on('mouseover', () => focus.style('display', null))
-        .on('mouseout', () => focus.style('display', 'none'))
+        .on('mouseover', () => {
+            focus.style('display', null);
+        })
+        .on('mouseout', () => {
+            focus.style('display', 'none');
+        })
         .on('mousemove', mousemove);
-
-    function mousemove() {
-        var x0 = x.invert(d3.mouse(this)[0]),
-            i = bisectDate(formattedData, x0, 1),
-            d0 = formattedData[i - 1],
-            d1 = formattedData[i],
-            d = x0 - parseTime(d0.date) > parseTime(d1.date) - x0 ? d1 : d0;
-        focus.attr('transform', 'translate(' + x(parseTime(d.date)) + ',' + y(d.price_usd) + ')');
-        focus.select('text').text(d.price_usd);
-        focus.select('.x-hover-line').attr('y2', height - y(d.price_usd));
-        focus.select('.y-hover-line').attr('x2', -x(parseTime(d.date)));
+    
     }
 
+    // Path Generator
+    line = d3.line()
+        .x((d) => x(d.date))
+        .y((d) => y(d[yValue]));
 
-    /******************************** Tooltip Code ********************************/
+    // Update Our Line Path
+    g.select('.line')
+        .transition(t)
+        .attr('d', line(dataTimeFiltered));
 
-});
+    // Update y-axis label
+    let newText = (yValue === 'price_usd') ? 'Price (USD)' :
+        ((yValue === 'market_cap') ? 'Market Capitalization (USD)' : '24 Hour Trading Volume (USD)')
+    yLabel.text(newText);
+}
 
+// Event Listeners
+$('#coin-select').on('change', update)
+$('#var-select').on('change', update)
